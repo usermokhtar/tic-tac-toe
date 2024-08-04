05fcbc5ea1df50d6dd48197e205340ee946ac656
usermokhtar <mokhtar.soumair.idrissi@gmail.com>
2024-08-04 18:47:38 +0100

    adding board to it

--- src/App.jsx
+++ src/App.jsx
@@ -3,6 +3,21 @@ function App() {
   return (
     <>
       <Header />
+      <main>
+        <div id='game-container'>
+          <ol id='players'>
+            <li>
+              <span className='player-name'>Player1</span>
+              <span className='player-symbol'>X</span>
+            </li>
+            <li>
+              <span className='player-name'>Player2</span>
+              <span className='player-symbol'>0</span>
+            </li>
+          </ol>
+          GAME BOARD
+        </div>
+      </main>
     </>
   );
 }
