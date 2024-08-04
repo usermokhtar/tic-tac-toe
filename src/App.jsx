fb5e0574d97abfaed01c0d92095bcb102183ba46
usermokhtar <mokhtar.soumair.idrissi@gmail.com>
2024-08-04 18:30:04 +0100

    adding header photo and title

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,10 @@
-
+import Header from './Components/Header';
 function App() {
-  
-
   return (
-    <h1>React Tic-Tac-Toe</h1>
-  )
+    <>
+      <Header />
+    </>
+  );
 }
 
-export default App
+export default App;
--- src/Components/Header.jsx
+++ src/Components/Header.jsx
@@ -1,7 +1,12 @@
 import React from 'react';
-
+import img1 from '../../public/game-logo.png';
 const Header = () => {
-  return <div>Header</div>;
+  return (
+    <header>
+      <img src={img1} alt='img1' />
+      <h1>React Tic-Tac-Toe</h1>
+    </header>
+  );
 };
 
 export default Header;
